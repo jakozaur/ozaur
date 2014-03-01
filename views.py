81b2c34f813c65c7af23ddb6d3d0e12480574056
Jacek Migdal <jacek@migdal.pl>
2014-02-28 20:52:37 -0800

    Add signup pages

--- views.py
+++ views.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def main_page():
+    return render_template("index.html")
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run()
