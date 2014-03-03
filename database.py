e3b6f8a9e743462ccd9667f7a742e3a2602c208d
Jacek Migdal <jacek@migdal.pl>
2014-03-02 19:20:52 -0800

    Define initial data schema

--- database.py
+++ database.py
@@ -1,20 +1,37 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, Index, ForeignKey, Text
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+from flask.ext.login import UserMixin
 
 import config
 
 engine = create_engine(config.DB_URL, encoding = "utf-8", echo = config.DB_DEBUG_ECHO)
 Base = declarative_base()
 
-class HelloWorld(Base):
-  __tablename__ = "hello_world"
+class User(Base, UserMixin):
+  __tablename__ = "user"
 
-  id = Column(Integer, primary_key=True)
-  name = Column(String)
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  email = Column(String(256), nullable=False)
+  display_name = Column(String(64), nullable=False)
+  headline = Column(String(128), nullable=False)
+  industry = Column(String(64), nullable=False)
+  location = Column(String(64), nullable=False)
+  interested_in = Column(String(256), nullable=False)
+  photo_url = Column(String(256))
 
-  def __repr__(self):
-    return "HelloWorld(%d, %s)" % (self.id, self.name)
+  __table_args__ = (Index("user_email_idx", "email", unique=True),)
 
+class Profile(Base):
+  __tablename__ = "profile"
+
+  id = Column(Integer, primary_key=True, autoincrement=True)
+  user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
+
+  external_key = Column(String(128))
+  data_json = Column(Text)
+
+  user = relationship("User", backref="profiles")
 
 def create_schema():
   Base.metadata.create_all(engine) 
