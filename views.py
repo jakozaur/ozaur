e0ac96c1c8343505b1f77c6e5fde7f8c5ac326ea
Jacek Migdal <jacek@migdal.pl>
2014-03-02 20:32:08 -0800

    Add fetching LinkedIn profile in backend

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,10 +10,14 @@ def main_page():
 def signup():
     return render_template("signup.html")
 
-@app.route("/create_profile", methods=["POST"])
-def create_profile():
+@app.route("/create_account", methods=["POST"])
+def create_account():
   linkedin_oauth_token = request.form["linkedin_oauth_token"]
-  return "Response " + linkedin_oauth_token
+  r = requests.get("https://api.linkedin.com/v1/people::(~):(id,first-name,last-name,headline,location,industry,num-connections,summary,specialties,positions,picture-url,public-profile-url,email-address,associations,interests,publications,patents,languages,skills,certifications,educations,courses,volunteer,num-recommenders,honors-awards)",
+      headers={"oauth_token": linkedin_oauth_token,
+        "x-li-format": "json"})
+
+  return "Response " + str(r.json())
 
 if __name__ == "__main__":
     app.run(debug=True)
