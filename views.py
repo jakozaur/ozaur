bef3c3bdf1f79697c8a7ff036c2e43f203a58917
Jacek Migdal <jacek@migdal.pl>
2014-03-02 20:22:12 -0800

    Pass token from LinkedIn to backend

--- views.py
+++ views.py
@@ -1,14 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def main_page():
     return render_template("index.html")
 
-@app.route('/signup')
+@app.route("/signup")
 def signup():
     return render_template("signup.html")
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/create_profile", methods=["POST"])
+def create_profile():
+  linkedin_oauth_token = request.form["linkedin_oauth_token"]
+  return "Response " + linkedin_oauth_token
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
